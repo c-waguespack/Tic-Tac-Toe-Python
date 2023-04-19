7909a1f952212389b8f0689991fde8ff92abbdcc
c-waguespack <99762405+c-waguespack@users.noreply.github.com>
2023-04-19 18:25:37 -0500

    Update helpertictactoe.py

--- helpertictactoe.py
+++ helpertictactoe.py
@@ -25,8 +25,3 @@ def checkIfWin(spots):
     else:
         return False
     
-#def main():
-    #spots = {1 : '1', 2 : '2', 3 : '3', 4 : '4', 5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9'}
-    #drawBoard(spots)
-    #checkTurn()
-#main()
